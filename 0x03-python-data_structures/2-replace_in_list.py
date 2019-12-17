05a56b77b42ae25467162f0fca04fd6178519299
Julian Villegas <julian.villegasplus@gmail.com>
2019-12-17 23:45:06 +0000

    [FIX]2-replace_in_list: else statement

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,6 +3,6 @@ def replace_in_list(my_list, idx, element):
     for arr in range(len(my_list)):
         if ((idx < 0) or (idx > len(my_list))):
             return (my_list)
-        elif (idx == arr):
-            my_list[arr] = element
+        else:
+            my_list[idx] = element
             return (my_list)
