63e6765346478eef19641218e4ba6d52eaa25fb3
Julian Villegas <julian.villegasplus@gmail.com>
2019-12-18 04:07:15 +0000

    [FIX]pep8 style

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,12 +4,12 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a = (0, 0)
     elif (len(tuple_a) == 1):
         tuple_a = (tuple_a[0], 0)
-    #a1, b1 = tuple_a
+    # a1, b1 = tuple_a
     if (len(tuple_b) == 0):
         tuple_b = (0, 0)
     elif (len(tuple_b) == 1):
         tuple_b = (tuple_b[0], 0)
-    #a2, b2 = tuple_b
+    # a2, b2 = tuple_b
     res1 = tuple_a[0] + tuple_b[0]
     res2 = tuple_a[1] + tuple_b[1]
     tuple_res = (res1, res2)
