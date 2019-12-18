f1f7cb269a19d58f01a1d92ce51eb122754a1d6e
Julian Villegas <julian.villegasplus@gmail.com>
2019-12-18 04:05:36 +0000

    [FIX]7-add_tuple: Changing values to tuplas

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,13 +4,13 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a = (0, 0)
     elif (len(tuple_a) == 1):
         tuple_a = (tuple_a[0], 0)
-    a1, b1 = tuple_a
+    #a1, b1 = tuple_a
     if (len(tuple_b) == 0):
         tuple_b = (0, 0)
     elif (len(tuple_b) == 1):
         tuple_b = (tuple_b[0], 0)
-    a2, b2 = tuple_b
-    res1 = a1 + a2
-    res2 = b1 + b2
+    #a2, b2 = tuple_b
+    res1 = tuple_a[0] + tuple_b[0]
+    res2 = tuple_a[1] + tuple_b[1]
     tuple_res = (res1, res2)
     return (tuple_res)
