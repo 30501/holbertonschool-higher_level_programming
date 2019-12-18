ec85fbd9944153f3e38e4029dec73d3816e7bd69
Julian Villegas <julian.villegasplus@gmail.com>
2019-12-18 04:18:14 +0000

    [FIX]8-multuple_returns: Fix None value in second position

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if (sentence == ''):
-        tuple_res = (None,)
+        tuple_res = (len(sentence), None)
     else:
         tuple_res = (len(sentence), sentence[0])
     return (tuple_res)
