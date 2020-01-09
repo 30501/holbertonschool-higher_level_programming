d3ecb5aa6a34bdc68a417d4b804c2a0545da966e
Julian Villegas <julian.villegasplus@gmail.com>
2020-01-09 05:09:58 +0000

    [FIX]101-safe_function: sending pointer to args instead value per value stored in local variables

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,10 +3,8 @@ import sys
 
 
 def safe_function(fct, *args):
-    a = args[0]
-    b = args[1]
     try:
-        result = fct(a, b)
+        result = fct(*args)
     except Exception as e:
         s = "Exception: " + str(e) + "\n"
         sys.stderr.write(s)
