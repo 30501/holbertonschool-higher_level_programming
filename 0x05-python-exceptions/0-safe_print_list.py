8c0f82490f1a6792bae234d67055d9302b5a19a3
Julian Villegas <julian.villegasplus@gmail.com>
2020-01-09 01:25:20 +0000

    [UPD]0-safe_print_list: optimize index in list loop

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    idx = 0
     count = 0
     if (x > 0):
         for i in range(x):
             try:
-                print("{:d}".format(my_list[idx]), end='')
-                idx += 1
+                print("{:d}".format(my_list[i]), end='')
                 count += 1
             except:
                 break
