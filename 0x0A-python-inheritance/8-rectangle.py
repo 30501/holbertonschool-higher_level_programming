c5ce3de0e576d6b69d403f634ca6b2eeeab313ab
Julian Villegas <julian.villegasplus@gmail.com>
2020-01-21 05:43:11 +0000

    [ADD]8-rectangle: class Rectangle that inherits from BaseGeometry (7-base_geometry.py)

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Base Geometry Class """
+
+
+class BaseGeometry:
+    """ class that improve geometry with integer validator"""
+    def area(self):
+        """ raises an Exception with the message area() is not implemented """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ validates value """
+        if not (isinstance(value, int)):
+            raise TypeError("{} must be an integer".format(name))
+        if (value <= 0):
+            raise ValueError("{} must be greater than 0".format(name))
+
+""" Program that creates a Rectangle and intance its values """
+
+
+class Rectangle(BaseGeometry):
+    """ class Rectangle that inherits from BaseGeometry """
+    def __init__(self, width, height):
+        """ Constructor """
+        self.__width = width
+        self.__height = height
+        BaseGeometry.integer_validator(self, "width", self.__width)
+        BaseGeometry.integer_validator(self, "height", self.__height)
