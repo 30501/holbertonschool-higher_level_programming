5d409706254991d52d7ab571df652299dbd18783
Julian Villegas <julian.villegasplus@gmail.com>
2020-01-21 06:11:39 +0000

    [FIX]8-rectangle: Changing order

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -23,7 +23,7 @@ class Rectangle(BaseGeometry):
     """ class Rectangle that inherits from BaseGeometry """
     def __init__(self, width, height):
         """ Constructor """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-        self.integer_validator("width", self.__width)
-        self.integer_validator("height", self.__height)
