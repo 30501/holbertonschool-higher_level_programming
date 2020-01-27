06903ebe4eac83f29dc71dfeb503a87a3b162db1
Julian Villegas <julian.villegasplus@gmail.com>
2020-01-27 18:59:44 +0000

    [FIX]base.py: changing logic in to_json_string(list_dictionaries) method 8

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,11 +18,13 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """ returns the JSON string representation of list_dictionaries """
-        if (list_dictionaries is None or len(list_dictionaries) == 0):
+        """
+        returns the JSON string representation of list_dictionaries
+        """
+        if list_dictionaries is None:
             return "[]"
         else:
-            return (json.dumps(list_dictionaries))
+            return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
