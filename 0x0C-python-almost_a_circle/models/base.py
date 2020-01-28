a0d0c9de16ead6765ea9fc54e5c216899434fa2b
Julian Villegas <julian.villegasplus@gmail.com>
2020-01-28 01:13:20 +0000

    [UPD]base.py: Update the class Base by adding the class methods def save_to_file_csv(cls, list_objs): and def load_from_file_csv(cls): that serializes and deserializes in CSV

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """ Base class of all other classes in this project"""
 import json
 import os
+import csv
 
 
 class Base:
@@ -67,4 +68,43 @@ class Base:
                 listjson = cls.from_json_string(stread)
                 for l in listjson:
                     my_list.append(cls.create(**l))
+        return (my_list)
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """ serializes list objects in csv format and save"""
+        fname = cls.__name__ + '.csv'
+        my_list = []
+        if (list_objs is not None):
+            for ins in list_objs:
+                dc = ins.to_dictionary()
+                if (cls.__name__ == 'Rectangle'):
+                    my_list.append([dc['id'], dc['width'], dc['height'],
+                                    dc['x'], dc['y']])
+                elif (cls.__name__ == 'Square'):
+                    my_list.append([dc['id'], dc['size'], dc['x'], dc['y']])
+                # print(my_list)
+        with open(fname, "w") as f:
+            wcsv = csv.writer(f)
+            wcsv.writerows(my_list)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """ deserializes csv of the list of instances """
+        my_list = []
+        dc = {}
+        fname = cls.__name__ + '.csv'
+        if os.path.isfile(fname):
+            with open(fname, 'r') as f:
+                csvread = csv.reader(f)
+                if (cls.__name__ == 'Rectangle'):
+                    keys = ['id', 'width', 'height', 'x', 'y']
+                elif (cls.__name__ == 'Square'):
+                    keys = ['id', 'size', 'x', 'y']
+                for row in csvread:
+                    aux = 0
+                    for i in row:
+                        dc[keys[aux]] = int(i)
+                        aux += 1
+                    my_list.append(cls.create(**dc))
         return (my_list)
