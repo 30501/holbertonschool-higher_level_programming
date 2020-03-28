be39e4ef1fc62ac628c196264c76f6fe64980781
Julian Villegas <julian.villegasplus@gmail.com>
2020-03-28 19:00:53 +0000

    [FIX]1-filter_states: Add * instead fields

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
                            db=argv[3], port=3306)
     cur = conn.cursor()
-    cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%' \
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' \
                  COLLATE latin1_general_cs ORDER BY id ASC;")
     query_rows = cur.fetchall()
 
