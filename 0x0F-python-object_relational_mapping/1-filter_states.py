0f5eb430146f1d2b3c56910992fc388dfe2d4529
Julian Villegas <julian.villegasplus@gmail.com>
2020-03-28 18:36:41 +0000

    [FIX]1-filter_states: Add COLLATE latin1_general_cs and delete charset utf8

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,10 +7,10 @@ from sys import argv
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
-                           db=argv[3], port=3306, charset="utf8")
+                           db=argv[3], port=3306)
     cur = conn.cursor()
     cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%' \
-                 ORDER BY id ASC;")
+                 COLLATE latin1_general_cs ORDER BY id ASC;")
     query_rows = cur.fetchall()
 
     for row in query_rows:
