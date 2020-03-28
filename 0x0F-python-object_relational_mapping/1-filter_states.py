2e122a0171399b964b1df8832a253540bb882ca8
Julian Villegas <julian.villegasplus@gmail.com>
2020-03-28 18:37:46 +0000

    [FIX]1-filter_states: Delete COLLATE latin1_general

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
                            db=argv[3], port=3306)
     cur = conn.cursor()
     cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%' \
-                 COLLATE latin1_general_cs ORDER BY id ASC;")
+                 ORDER BY id ASC;")
     query_rows = cur.fetchall()
 
     for row in query_rows:
