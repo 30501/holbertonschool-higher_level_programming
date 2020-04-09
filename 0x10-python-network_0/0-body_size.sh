043d2b11861635fb0ff23eebb84708fe3926f49d
Julian Villegas <julian.villegasplus@gmail.com>
2020-04-09 03:10:34 +0000

    [FIX]0-body_size: delete two lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
-url="$1"
-curl -so /dev/null "$url" -w '%{size_download}\n'
+curl -so /dev/null "$1" -w '%{size_download}\n'
