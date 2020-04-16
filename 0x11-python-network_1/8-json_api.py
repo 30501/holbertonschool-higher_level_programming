2469bb5de3c305e32edefde69526decb97060893
Julian Villegas <julian.villegasplus@gmail.com>
2020-04-16 04:42:26 +0000

    [FIX]8-json_api: Solving () in len function

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,7 +5,7 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5000/search_user"
+    url = 'http://0.0.0.0:5000/search_user'
     if (len(argv) > 1):
         letter = argv[1]
     else:
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     req = requests.post(url, data=payload)
     try:
         json_body = req.json()
-        if (len(json_body != 0)):
+        if (len(json_body) != 0):
             print("[{}] {}".format(json_bod['id'], json_bod['name']))
         else:
             print("No result")
