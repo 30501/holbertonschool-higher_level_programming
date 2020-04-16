0c88810c7fc0bf37921fccfb675c095046f25585
Julian Villegas <julian.villegasplus@gmail.com>
2020-04-16 04:45:28 +0000

    [FIX]8-json_api: Add ValueError except

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -18,5 +18,5 @@ if __name__ == "__main__":
             print("[{}] {}".format(json_bod['id'], json_bod['name']))
         else:
             print("No result")
-    except:
+    except ValueError:
         print("Not a valid JSON")
