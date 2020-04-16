b6c3f3693f77b21109c793052a412e05c5fe3756
Julian Villegas <julian.villegasplus@gmail.com>
2020-04-16 04:07:28 +0000

    [FIX]6-post_email: Delete string in email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,6 +7,6 @@ from sys import argv
 if __name__ == "__main__":
     url = argv[1]
     email = argv[2]
-    payload = {'email': 'email'}
+    payload = {'email': email}
     req = requests.post(url, data=payload)
     print(req.text)
