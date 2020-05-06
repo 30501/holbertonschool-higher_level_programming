795d600774016eaf0741a070711dde262578d333
Julian Villegas <julian.villegasplus@gmail.com>
2020-05-06 03:37:09 +0000

    [FIX]11-second_biggest: change conditional

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 // script that searches the second biggest integer in the list of argument
 const args = process.argv.slice(2);
-if (!args[0]) {
-  console.log('0');
-} else if (args.length === 1) {
+if (args.length === 0 || args.length === 1) {
   console.log('0');
 } else {
   console.log(args.sort().reverse()[1]);
