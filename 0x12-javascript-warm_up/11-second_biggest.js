5d120b790af0f9d6d79c6198acaae15714895903
Julian Villegas <julian.villegasplus@gmail.com>
2020-05-06 03:33:22 +0000

    [FIX]11-second_biggest: assign string in console.log

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,9 +2,9 @@
 // script that searches the second biggest integer in the list of argument
 const args = process.argv.slice(2);
 if (!args[0]) {
-  console.log(0);
+  console.log('0');
 } else if (args.length === 1) {
-  console.log(0);
+  console.log('0');
 } else {
   console.log(args.sort().reverse()[1]);
 }
