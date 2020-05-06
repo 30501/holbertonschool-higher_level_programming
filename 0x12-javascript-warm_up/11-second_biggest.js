aee1b3fb817d82ae673456d26c958f6247f5ae80
Julian Villegas <julian.villegasplus@gmail.com>
2020-05-06 03:46:21 +0000

    [FIX]11-second_biggest: parse all elements to int

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 // script that searches the second biggest integer in the list of argument
 const args = process.argv.slice(2);
+let argsInt = args.map(s => parseInt(s));
 if (args.length === 0 || args.length === 1) {
   console.log('0');
 } else {
-  console.log(args.sort().reverse()[1]);
+  console.log(argsInt.sort().reverse()[1]);
 }
