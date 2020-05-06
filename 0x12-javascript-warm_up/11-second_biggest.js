3ec381a392caa97f7b2248571e260fd0ce0cb344
Julian Villegas <julian.villegasplus@gmail.com>
2020-05-06 03:47:46 +0000

    [FIX]11-second_biggest: change let by conts

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // script that searches the second biggest integer in the list of argument
 const args = process.argv.slice(2);
-let argsInt = args.map(s => parseInt(s));
+const argsInt = args.map(s => parseInt(s));
 if (args.length === 0 || args.length === 1) {
   console.log('0');
 } else {
